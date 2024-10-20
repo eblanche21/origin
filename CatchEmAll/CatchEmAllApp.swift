66815781f1fa49c3415332d730533aea1a0891bd
Ethan Blanche <blancheq@bc.edu>
2024-10-20 17:42:42 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Ethan Blanche on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchEmAll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatchEmAll
+//
+//  Created by Ethan Blanche on 10/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
